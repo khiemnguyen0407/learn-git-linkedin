ce374fb589a14bbfc58a215898227581ad486eea
Cafesua <khiem.nguyen@glasgow.ac.uk>
2025-10-27 17:59:34 +0000

    Edit plot-periodic.py

--- plot-periodic.py
+++ plot-periodic.py
@@ -3,7 +3,7 @@ import numpy as np
 import matplotlib.pyplot as plt
 
 x = np.linspace(0, 2*np.pi, 256)
-f = np.exp(-np.sin(x) + np.cos(2*x))
+f = np.exp(-np.sin(x) + np.cos(2*x) + np.cos(3*x))
 plt.plot(x, f)
 plt.grid(True)
 plt.show()
