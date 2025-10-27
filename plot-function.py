7bffa78a63dcd71f193575cb68c78f5c0af3dc2f
Cafesua <khiem.nguyen@glasgow.ac.uk>
2025-10-27 13:58:42 +0000

    update plot-function.py

--- plot-function.py
+++ plot-function.py
@@ -3,7 +3,15 @@ import numpy as np
 import matplotlib.pyplot as plt
 
 x = np.linspace(0, 1, 100)
-f = np.exp(-np.sin(2*np.pi*x))
-plt.plot(x, f)
+f1 = np.exp(-np.sin(2*np.pi*x))
+f2 = np.exp(-np.cos(2*np.pi*x))
+plt.subplots(nrows=2, ncols=1, sharex=True)
+plt.subplot(211)
+plt.plot(x, f1)
+plt.grid(True)
+
+plt.subplot(212)
+plt.plot(x, f2)
 plt.grid(True)
 plt.show()
+# %%
